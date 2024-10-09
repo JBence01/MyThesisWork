765b01de406410abb9adaad386e5ff60fadee860
JBence01 <jonas.bence11@gmail.com>
2024-10-09 18:09:48 +0200

    chart commit

--- app/src/main/java/hu/unideb/inf/aic/KorabbiActivity.java
+++ app/src/main/java/hu/unideb/inf/aic/KorabbiActivity.java
@@ -10,6 +10,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
@@ -17,6 +19,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -29,6 +32,11 @@ public class KorabbiActivity extends AppCompatActivity {
     Button buttonAktualis;
     private DBkezelo DBkezelo;
     private LineChart chart;
+    CheckBox tempCheckBox, humCheckBox, soilCheckBox;
+
+    private boolean showTemperature = true;
+    private boolean showHumidity = true;
+    private boolean showSoilMoisture = true;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +46,34 @@ public class KorabbiActivity extends AppCompatActivity {
         buttonAktualis = findViewById(R.id.buttonAktualis);
         chart = findViewById(R.id.chart);
         DBkezelo = new DBkezelo(this);
-        
+        tempCheckBox = findViewById(R.id.tempCheckBox);
+        humCheckBox = findViewById(R.id.humCheckBox);
+        soilCheckBox = findViewById(R.id.soilCheckBox);
+
+        tempCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                showTemperature = isChecked; // Állapot beállítása
+                diagramRajzolas();           // Diagram újrarajzolása
+            }
+        });
+
+        humCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                showHumidity = isChecked;
+                diagramRajzolas();
+            }
+        });
+
+        soilCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                showSoilMoisture = isChecked;
+                diagramRajzolas();
+            }
+        });
+
         buttonAktualis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,13 +112,37 @@ public class KorabbiActivity extends AppCompatActivity {
 
                 LineDataSet homersekletDataSet = new LineDataSet(homersekletEntries, "Hőmérséklet");
                 homersekletDataSet.setColor(Color.RED);
+                homersekletDataSet.setValueTextSize(10f);
                 LineDataSet paratartalomDataSet = new LineDataSet(paratartalomEntries, "Páratartalom");
                 paratartalomDataSet.setColor(Color.BLUE);
+                paratartalomDataSet.setValueTextSize(10f);
                 LineDataSet talajnedvessegDataSet = new LineDataSet(talajnedvessegEntries, "Talajnedvesség");
                 talajnedvessegDataSet.setColor(Color.GREEN);
+                talajnedvessegDataSet.setValueTextSize(10f);
 
-                LineData lineData = new LineData(homersekletDataSet, paratartalomDataSet, talajnedvessegDataSet);
+                LineData lineData = new LineData();
+                if (showTemperature) {
+                    lineData.addDataSet(homersekletDataSet);
+                }
+                if (showHumidity) {
+                    lineData.addDataSet(paratartalomDataSet);
+                }
+                if (showSoilMoisture) {
+                    lineData.addDataSet(talajnedvessegDataSet);
+                }
                 chart.setData(lineData);
+
+                // Tengely feliratainak szövegmérete
+                chart.getXAxis().setTextSize(14f);
+                chart.getAxisLeft().setTextSize(14f);
+                chart.getAxisRight().setTextSize(14f);
+
+                // Legend (magyarázat) kikapcsolása a checkboxok miatt
+                chart.getLegend().setEnabled(false);
+
+                //Egy kis extra hely a diagram körül, hogy a szöveg ne vágodjon le
+                chart.setExtraOffsets(10, 10, 10, 10);
+
                 chart.invalidate();
             }
         });
